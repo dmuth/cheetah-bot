31ddda66803c429cbd483f12f04cfdd864119f7c
Douglas Muth <dmuth@dmuth.org>
2021-05-03 22:11:56 -0400

    Finished refactoring

--- cheetah-bot.py
+++ cheetah-bot.py
@@ -10,27 +10,13 @@ import json
 import logging
 import re
 import sys
-import time
-import traceback
 sys.path.append("lib")
 
-import telegram
-from telegram.error import TelegramError
-from telegram.ext import Updater
-from telegram.ext import CommandHandler, MessageHandler, Filters
 
 import match
-from lib.counters import Counters
-from lib.filter.filter import Filter
-from lib.filter.profanity import Profanity
-from lib.match import Match
-from lib.rate_limiters import RateLimiters
-from lib.replies import Replies
-from lib.sleep_wake import SleepWake
 from lib.cheetah_bot import CheetahBot
 
 
-cheetah_bot = CheetahBot()
 
 
 #
@@ -65,188 +51,10 @@ args = parser.parse_args()
 #print(args) # Debugging
 
 
+cheetah_bot = CheetahBot()
+cheetah_bot.start(args.token, args.quotes_file, args.images_file, 
+	args.group_ids, args.group_names, args.actions, args.period, args.reply_every)
 
-
-
-
-#
-# Respond to a /start command
-#
-def start(update, context):
-	logger.info(f"chat_id: {update.effective_chat.id}, text={text}")
-	context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-
-
-
-#
-# This is a wrapper which returns our actual handler.
-# The reason for this is so that the variables we need can be in-scope, 
-# without having to make them globals.
-# This will make it easier to turn these functions into a class in the future.
-#
-def echo_wrapper(my_id, my_username, allowed_group_ids, allowed_group_names, actions, period, reply_every_n,
-	replies):
-
-	global cheetah_bot
-
-	counters = Counters(reply_every_n)
-	filter = Filter()
-	match = Match()
-	profanity = Profanity()
-	rate_limiters = RateLimiters(actions, period)
-	sleep_wake = SleepWake()
-
-
-	#
-	# Our handler that is fired when a message comes in
-	#
-	def echo_core(update, context):
-
-		# Set some defaults
-		reply = ""
-
-		# Filter newlines out of our message for logging and matching purposes
-		message = update.message
-		text = ""
-		if update.message.text:
-			text = update.message.text.replace("\r", " ").replace("\n", " ")
-
-		# How old is the message?
-		age = time.time() - message.date.timestamp()
-
-		logger.info(f"New Message: age={age:.3f}, chat_id={update.effective_chat.id}, text={text[0:30]}...")
-		#logger.info(f"Update: {update}") # Debugging
-		#logger.info(f"Message: {message}") # Debugging
-		#logger.info(f"Effective chat: {update.effective_chat}") # Debugging
-
-		# Bail if the message is too old, otherwise we risk spamming groups after a hiatus
-		if age > 10:
-			logger.info(f"Message is {age} > 10 seconds old, ignoring.")
-			return(None)
-
-
-		# Was this a bot add/remove?
-		if filter.messageIsIgnorable(update, message, my_id):
-			return(None)
-
-		# Was this a DM?
-		if filter.messageIsDm(update):
-			logger.info("This is a DM, bailing out (for now...)")
-			text = "You must message me in an approved group."
-			context.bot.send_message(chat_id = update.effective_chat.id, text = text)
-			return(None)
-
-		#
-		# Bail out if we're not in an allowed group
-		#
-		chat_id = update.effective_chat.id
-		chat_name = update.effective_chat.title
-		if not match.doesGroupMatch(allowed_group_ids, allowed_group_names, chat_id, chat_name):
-			return(None)
-
-		#
-		# See if anyone in the chat said "cheetah" or "chee"
-		#
-		if filter.messageContainsChee(text):
-			reply = filter.messageContainsCheeReply(text)
-			logger.info("String 'chee' detected")
-
-		#
-		# I'm not thrilled about calling checkForFoulLanguage() twice, but Python
-		# doesn't let me do "if (value = func())" syntax like other languages do.
-		# Once this goes into a class, I can have the function just set a classwide value instead.
-		#
-		if profanity.hasFoulLanguage(update, text):
-			reply = profanity.getReply()
-			logger.info("Profanity detected")
-
-		#
-		# Get our rate limiter for this chat
-		# 
-		limiter = rate_limiters.getRateLimiter(chat_id)
-
-		#
-		# If the message wasn't to the bot, and we're not replying to a user, stop.
-		#
-		if not reply:
-			if not match.doesUserMatch(my_id, my_username, update.message, text):
-				#
-				# See if we should reply and do so.
-				#
-				should_reply = counters.update(chat_id)
-				if should_reply:
-					cheetah_bot.sendRandomReply(context.bot, limiter, sleep_wake, replies, chat_id,
-						message_id = message.message_id)
-				return(None)
-
-		#
-		# If we already have a reply, then send it out.
-		# Otherwise, we have no idea what the user is talking about, so let's just
-		# grab a random string of text or URL.
-		#
-		if reply:
-			cheetah_bot.sendMessage(context.bot, limiter, sleep_wake, chat_id, reply = reply, 
-				message_id = message.message_id)
-		else:
-			cheetah_bot.sendRandomReply(context.bot, limiter, sleep_wake, replies, chat_id, 
-				message_id = message.message_id)
-
-
-	return(echo_core)
-
-
-
-#
-# Our main entrypoint
-#
-def main(args):
-
-	#print(args)
-	global cheetah_bot
-
-	replies = Replies(args.quotes_file, args.images_file)
-
-	bot = telegram.Bot(token = args.token)
-	logger.info(f"Successfully authenticated! {bot.get_me()}")
-	my_username = bot.get_me().username
-	my_id = bot.get_me().id
-	logger.info(f"My usernamne: {my_username}, My ID: {my_id}")
-
-	allowed_group_ids = cheetah_bot.getAllowedIds(args.group_ids)
-	logger.info(f"Allowed Group IDs: {allowed_group_ids}")
-	allowed_group_names = cheetah_bot.getAllowedIds(args.group_names)
-	logger.info(f"Allowed Group Names: {allowed_group_names}")
-
-	updater = Updater(token = args.token)
-	dispatcher = updater.dispatcher
-
-	#
-	# Catch errors
-	#
-	dispatcher.add_error_handler(cheetah_bot.errorHandler)
-
-	#
-	# Uncomment this if I ever want a /start command for some reason.
-	#
-	#start_handler = CommandHandler('start', start)
-	#dispatcher.add_handler(start_handler)
-
-	#
-	# We're just gonna reply to everything.
-	#
-	cb = echo_wrapper(my_id, my_username, allowed_group_ids, allowed_group_names, 
-		args.actions, args.period, args.reply_every, replies)
-	echo_handler = MessageHandler(Filters.all, cb)
-
-	dispatcher.add_handler(echo_handler)
-
-	updater.start_polling()
-
-
-#
-# Start the bot!
-# 
-main(args)
 
 
 
--- lib/cheetah_bot.py
+++ lib/cheetah_bot.py
@@ -2,8 +2,21 @@
 
 import logging
 import random
+import time
 
+from counters import Counters
+from filter.filter import Filter
+from filter.profanity import Profanity
+from match import Match
+from rate_limiters import RateLimiters
+from replies import Replies
+from sleep_wake import SleepWake
+
+import telegram
 from telegram.ext import CallbackContext
+from telegram.ext import CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater
+from telegram.error import TelegramError
 
 logger = logging
 
@@ -13,9 +26,163 @@ logger = logging
 #
 class CheetahBot():
 
+	counters = None
+	filter = None
+	match = None
+	profanity = None
+	rate_limiters = None
+	replies = None 
+	sleep_wake = None
+
 
 	def __init__(self):
 		pass
+
+
+	#
+	# Our main entry point to start bot.  This function will never exit.
+	#
+	def start(self, token, quotes_file, images_file, group_ids, group_names, 
+		actions, period, reply_every):
+
+		self.counters = Counters(reply_every)
+		self.filter = Filter()
+		self.match = Match()
+		self.profanity = Profanity()
+		self.rate_limiters = RateLimiters(actions, period)
+		self.replies = Replies(quotes_file, images_file)
+		self.sleep_wake = SleepWake()
+
+		self.allowed_group_ids = self.getAllowedIds(group_ids)
+		logger.info(f"Allowed Group IDs: {self.allowed_group_ids}")
+		self.allowed_group_names = self.getAllowedIds(group_names)
+		logger.info(f"Allowed Group Names: {self.allowed_group_names}")
+
+		bot = telegram.Bot(token = token)
+		logger.info(f"Successfully authenticated! {bot.get_me()}")
+		self.my_username = bot.get_me().username
+		self.my_id = bot.get_me().id
+		logger.info(f"My usernamne: {self.my_username}, My ID: {self.my_id}")
+
+		updater = Updater(token = token)
+		dispatcher = updater.dispatcher
+
+		#
+		# Catch errors
+		#
+		dispatcher.add_error_handler(self.errorHandler)
+
+		#
+		# We're just gonna reply to everything.
+		#
+		#cb = echo_wrapper(my_id, my_username, allowed_group_ids, allowed_group_names, 
+		#	args.actions, args.period, args.reply_every, replies)
+		#cb = cheetah_bot.echo_wrapper(my_id, my_username, allowed_group_ids, allowed_group_names, 
+			#args.actions, args.period, args.reply_every, replies)
+		#echo_handler = MessageHandler(Filters.all, cb)
+
+		echo_handler = MessageHandler(Filters.all, self.echo)
+		dispatcher.add_handler(echo_handler)
+
+		updater.start_polling()
+
+
+
+	#
+	# Our handler that is fired when a message comes in
+	#
+	def echo(self, update, context):
+
+		# Set some defaults
+		reply = ""
+
+		# Filter newlines out of our message for logging and matching purposes
+		message = update.message
+		text = ""
+		if update.message.text:
+			text = update.message.text.replace("\r", " ").replace("\n", " ")
+
+		# How old is the message?
+		age = time.time() - message.date.timestamp()
+
+		logger.info(f"New Message: age={age:.3f}, chat_id={update.effective_chat.id}, text={text[0:30]}...")
+		#logger.info(f"Update: {update}") # Debugging
+		#logger.info(f"Message: {message}") # Debugging
+		#logger.info(f"Effective chat: {update.effective_chat}") # Debugging
+
+		# Bail if the message is too old, otherwise we risk spamming groups after a hiatus
+		if age > 10:
+			logger.info(f"Message is {age} > 10 seconds old, ignoring.")
+			return(None)
+
+		# Was this a bot add/remove?
+		if self.filter.messageIsIgnorable(update, message, self.my_id):
+			return(None)
+
+		# Was this a DM?
+		if self.filter.messageIsDm(update):
+			logger.info("This is a DM, bailing out (for now...)")
+			text = "You must message me in an approved group."
+			context.bot.send_message(chat_id = update.effective_chat.id, text = text)
+			return(None)
+
+		#
+		# Bail out if we're not in an allowed group
+		#
+		chat_id = update.effective_chat.id
+		chat_name = update.effective_chat.title
+		if not self.match.doesGroupMatch(self.allowed_group_ids, self.allowed_group_names, 
+			chat_id, chat_name):
+			return(None)
+
+		#
+		# See if anyone in the chat said "cheetah" or "chee"
+		#
+		if self.filter.messageContainsChee(text):
+			reply = self.filter.messageContainsCheeReply(text)
+			logger.info("String 'chee' detected")
+
+		#
+		# I'm not thrilled about calling checkForFoulLanguage() twice, but Python
+		# doesn't let me do "if (value = func())" syntax like other languages do.
+		# Once this goes into a class, I can have the function just set a classwide value instead.
+		#
+		if self.profanity.hasFoulLanguage(update, text):
+			reply = self.profanity.getReply()
+			logger.info("Profanity detected")
+
+		#
+		# Get our rate limiter for this chat
+		# 
+		limiter = self.rate_limiters.getRateLimiter(chat_id)
+
+		#
+		# If the message wasn't to the bot, and we're not replying to a user, stop.
+		#
+		if not reply:
+			if not self.match.doesUserMatch(self.my_id, self.my_username, update.message, text):
+				#
+				# See if we should reply and do so.
+				#
+				should_reply = self.counters.update(chat_id)
+				if should_reply:
+					self.sendRandomReply(context.bot, limiter, 
+						chat_id,
+						message_id = message.message_id)
+				return(None)
+
+		#
+		# If we already have a reply, then send it out.
+		# Otherwise, we have no idea what the user is talking about, so let's just
+		# grab a random string of text or URL.
+		#
+		if reply:
+			self.sendMessage(context.bot, limiter, chat_id, reply = reply, 
+				message_id = message.message_id)
+		else:
+			self.sendRandomReply(context.bot, limiter, chat_id, 
+				message_id = message.message_id)
+
 
 
 	#
@@ -53,15 +220,15 @@ class CheetahBot():
 	#
 	# Figure out which reply function to use, get a reply, and send it off!
 	#
-	def sendRandomReply(self, bot, limiter, sleep_wake, replies, chat_id, message_id):
+	def sendRandomReply(self, bot, limiter, chat_id, message_id):
 
 		if random.randint(0, 1):
-			reply = replies.getRandomMessageText()
-			self.sendMessage(bot, limiter, sleep_wake, chat_id, 
+			reply = self.replies.getRandomMessageText()
+			self.sendMessage(bot, limiter, chat_id, 
 				reply = reply, message_id = message_id)
 		else:
-			reply = replies.getRandomMessageImage()
-			self.sendMessage(bot, limiter, sleep_wake, chat_id, 
+			reply = self.replies.getRandomMessageImage()
+			self.sendMessage(bot, limiter, chat_id, 
 				image_url = reply["url"], caption = reply["caption"],
 				message_id = message_id)
 
@@ -70,7 +237,7 @@ class CheetahBot():
 	#
 	# Send a message in a way that honors our rate limiter's quota.
 	#
-	def sendMessage(self, bot, limiter, sleep_wake, chat_id, 
+	def sendMessage(self, bot, limiter, chat_id, 
 		reply = None, image_url = None, caption = None, message_id = None):
 
 		if limiter.action():
@@ -100,7 +267,7 @@ class CheetahBot():
 			# Yes, this will only work with one group, even if we are listening in multiple groups.
 			#
 			if limiter.isQuotaExhausted():
-				sleep_wake.goToSleep(bot, limiter, chat_id)
+				self.sleep_wake.goToSleep(bot, limiter, chat_id)
 
 		else:
 			logger.info(f"Not sending message, quota currently exhausted. quota_left={limiter.getQuota():.3f}")
